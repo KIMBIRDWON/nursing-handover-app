4561504e113fecb4c186c446239470292b6a0dba
KIMBIRDWON <pswk0907@gmail.com>
2025-05-08 20:45:42 +0900

    menu 생성(드로어 뷰)

--- app/src/main/java/com/example/nursing_handover_app/LoginActivity.kt
+++ app/src/main/java/com/example/nursing_handover_app/LoginActivity.kt
@@ -5,13 +5,15 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.nursing_handover_app.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        val binding = ActivityLoginBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_login)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        setContentView(binding.root)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main_login)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
--- app/src/main/java/com/example/nursing_handover_app/MainActivity.kt
+++ app/src/main/java/com/example/nursing_handover_app/MainActivity.kt
@@ -1,39 +1,53 @@
 package com.example.nursing_handover_app
 
 import android.os.Bundle
+import android.view.MenuItem
 import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import androidx.viewpager2.widget.ViewPager2
 import com.example.nursing_handover_app.databinding.ActivityMainBinding
-import com.google.android.material.tabs.TabLayout
 import com.google.android.material.tabs.TabLayoutMediator
 
 class MainActivity : AppCompatActivity() {
+    lateinit var toggle: ActionBarDrawerToggle
     override fun onCreate(savedInstanceState: Bundle?) {
         val binding = ActivityMainBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(binding.root)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.drawer)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-        val tabLayout = findViewById<TabLayout>(R.id.tab)
-        val viewPager = findViewById<ViewPager2>(R.id.viewpager)
-        val adapter = MyPagerAdapter(this)
-        viewPager.adapter = adapter
+        setSupportActionBar(binding.toolbar)
+
+        //toolbar와 DrawerLayout 연동(햄버거 아이콘 설정)
+        toggle = ActionBarDrawerToggle(this, binding.drawer, binding.toolbar, R.string.drawer_open, R.string.drawer_close
+        ).apply {
+            binding.drawer.addDrawerListener(this)
+            syncState()
+        }
+
+        binding.viewpager.adapter = MyPagerAdapter(this)
 
         //Tab과 ViewPager2 연결
-        TabLayoutMediator(tabLayout, viewPager) { tab, position ->
+        TabLayoutMediator(binding.tab, binding.viewpager) { tab, position ->
             tab.text = when (position) {
-                0 -> "메인화면"
+                0 -> "홈"
                 1 -> "작성"
                 2 -> "조회"
                 else -> ""
             }
         }.attach()
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if(toggle.onOptionsItemSelected(item)){
+            return true
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
